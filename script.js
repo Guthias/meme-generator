d52d76a615e968e1c9de1985a0147b2715c0b740
guthias <gmgustavom@gmail.com>
2021-11-27 23:47:31 -0300

    Adicionando classes para mudança de borda

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const memeImage = document.getElementById('meme-image');
 const memeInsert = document.getElementById('meme-insert');
 const memeInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeContainer = document.getElementById('meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+
+function changeBorder(event) {
+  const borderName = event.target.id;
+  const regex = /earth|fire|water/g;
+  const currentClasses = memeContainer.className;
+  if (currentClasses.match(regex)) {
+    memeContainer.className = currentClasses.replace(regex, borderName);
+  } else {
+    memeContainer.classList.add(borderName);
+  }
+}
 
 function changeImage(event) {
   // Source: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
@@ -15,3 +30,6 @@ function changeText(event) {
 
 memeInsert.addEventListener('change', changeImage);
 memeInput.addEventListener('keyup', changeText);
+fireButton.addEventListener('click', changeBorder);
+waterButton.addEventListener('click', changeBorder);
+earthButton.addEventListener('click', changeBorder);
