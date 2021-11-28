5f878eab047894e50ac0b0c10441036792e0cc44
guthias <gmgustavom@gmail.com>
2021-11-27 21:22:34 -0300

    Adicionando texto quando o usuario escreve

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const memeImage = document.getElementById('meme-image');
 const memeInsert = document.getElementById('meme-insert');
+const memeInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
 
 function changeImage(event) {
   // Source: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
@@ -7,4 +9,9 @@ function changeImage(event) {
   memeImage.src = url;
 }
 
+function changeText(event) {
+  memeText.innerText = event.target.value;
+}
+
 memeInsert.addEventListener('change', changeImage);
+memeInput.addEventListener('keyup', changeText);
